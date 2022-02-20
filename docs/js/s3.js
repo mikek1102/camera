729ecc40788ac210f647f40268466ac9fae7a11d
mikek1102 <mkennedy@greencheckverified.com>
2022-02-20 13:46:57 -0500

    combine s3 upload into camera button

--- docs/js/s3.js
+++ docs/js/s3.js
@@ -15,7 +15,7 @@
         });
         
         function s3upload() {  
-            var files = document.getElementById('fileUpload').files;
+            var files = document.getElementById('camera').files;
             if (files) 
             {
                 var file = files[0];
